2287de0b0bfa0321bdb1c1cfec1dd0734f9a25a1
Marco Sinigaglia <marco.sinigaglia@techdata.com>
2022-03-07 09:52:08 +0100

    removed xfce

--- aziotcia.sh
+++ aziotcia.sh
@@ -33,26 +33,6 @@ APP_ID=$(az iot central app list -g $RESOURCE_GROUP_NAME | grep application | aw
 
 az iot central user create --user-id $USER_OBJECT_ID --app-id $APP_ID --email $USER_EMAIL --role admin
 
-# Install Desktop
-echo $(date) " linux-install-desktop - Starting Script"
-sudo apt install ubuntu-gnome-desktop -y
-
-# Install xrdp
-echo $(date) " linux-install-xrdp - Starting Script"
-sudo apt install xrdp -y
-sudo systemctl enable xrdp
-
-
-# Install xfce4
-echo $(date) " linux-install-xfce4 - Starting Script"
-sudo apt-get install xfce4 -y
-
-echo xfce4-session > /home/$ADMIN_USER/.xsession 
-sed -i '8d' /etc/xrdp/startwm.sh
-
-echo "startxfce4" >> /etc/xrdp/startwm.sh
-sudo service xrdp restart
-
 
 
 echo "Setting up your mobiusflow cloud instance..."
